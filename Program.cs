1c4b698e7f106dec012d6364a182623313e04c97
sagar <sagar@SAGARPC>
2025-01-10 20:32:10 +0530

    nlog added

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 ﻿using potatoconsumer;
-
+using NLog;
 internal class Program
 {
     private static void Main(string[] args)
     {
+        var logger = NLog.LogManager.GetCurrentClassLogger();
+        var a = "🫎";
+        logger.Info("in main fn 🌵 {0}", a);
+
         kafkaconscl ob = new kafkaconscl();
         ob.consfn();
-        
     }
 }
+
+
+/*
+> dotnet add package NLog --version 5.3.4
+*/
